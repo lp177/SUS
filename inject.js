63306b1883af4ff658483bd3edd3fe4f5cac0099
lp177 <lucperez177@gmail.com>
2021-04-03 07:12:46 +0200

    add franceinter management

--- inject.js
+++ inject.js
@@ -51,7 +51,23 @@ function extractYoutubeWithCurrentTime()
 	s.id = 'script177';
 	s.innerText = "const getVideoUrl177 = (document.getElementById('movie_player')) ? document.getElementById('movie_player').getVideoUrl() : null;document.body.setAttribute( 'getvideourl177', getVideoUrl177 )";
 	(document.head||document.documentElement).appendChild(s);
-	return document.body.getAttribute( 'getvideourl177' );
+	const video_url = document.body.getAttribute( 'getvideourl177' );
+	return (video_url==='null'||video_url==='https://www.youtube.com/watch')?window.location.href:video_url;
+}
+
+function extractGenericAudioWithCurrentTime(timelineSelector,timelineElementToSeconds=(element)=>parseFloat(element.value)*60)
+{
+	const audioElement = document.querySelector('audio'),timelineElement = document.querySelector(timelineSelector);
+
+	if (!audioElement||!timelineElement)
+		return console.warn('Fail to found current track metadata');
+	const current_time = timelineElementToSeconds(timelineElement);
+	if (!current_time)
+		return;
+	const urlGenerated = audioElement.src + '#t=' + current_time;
+	if (!purposeUrl(urlGenerated))
+		return;
+	return urlGenerated;
 }
 
 function extractURL()
@@ -64,6 +80,16 @@ function extractURL()
 		generatedUrl = extractTwitchWithCurrentTime();
 	else if (window.location.href.startsWith('https://youtube.com/') || window.location.href.startsWith('https://www.youtube.com/') || window.location.href.startsWith('https://m.youtube.com/'))
 		generatedUrl = extractYoutubeWithCurrentTime();
+	else if (window.location.href.startsWith('https://www.franceinter.fr/'))
+		generatedUrl = extractGenericAudioWithCurrentTime('.time-informations', (timelineElement)=>{
+			const times = timelineElement.innerText.split(' ')[0].split(':');
+			if (times.length>2)
+				return (parseInt(times[0])*60*60)+(parseInt(times[1])*60)+parseInt(times[2]);
+			else if (times.length==2)
+				return parseInt(times[0])*60+parseInt(times[1]);
+			return parseInt(times[0]);
+
+		});
 
 	if (!generatedUrl)
 	{
